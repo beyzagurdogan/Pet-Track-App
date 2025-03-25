81a20612489f74dde8271808a72957ad38b09b5d
beyzagurdogan <beyzae75@gmail.com>
2025-03-25 17:48:52 +0100

    Initial Commit

---
+++ PetTrack/PetTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetTrackApp.swift
+//  PetTrack
+//
+//  Created by Beyza Gurdogan on 25.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct PetTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PetTrackTests/PetTrackTests.swift
@@ -0,0 +1,17 @@
+//
+//  PetTrackTests.swift
+//  PetTrackTests
+//
+//  Created by Beyza Gurdogan on 25.03.2025.
+//
+
+import Testing
+@testable import PetTrack
+
+struct PetTrackTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
